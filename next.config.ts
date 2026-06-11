d0747fce14e47fb77e2615623aed937ec82accfc
MOHAMED ALAA <ashmawyalaa@gmail.com>
2026-06-11 17:55:59 +0300

    fix: remove withSentryConfig wrapper to bypass vercel middleware trace error

--- next.config.ts
+++ next.config.ts
@@ -5,27 +5,4 @@ const nextConfig: NextConfig = {
   serverExternalPackages: ["@prisma/client", "pg"],
 };
 
-export default withSentryConfig(nextConfig, {
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options
-
-  org: process.env.SENTRY_ORG,
-  project: process.env.SENTRY_PROJECT,
-
-  // Only print logs for uploading source maps in CI
-  silent: !process.env.CI,
-
-  // For all available options, see:
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-  // Upload a larger set of source maps for prettier stack traces (increases build time)
-  widenClientFileUpload: true,
-
-  // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-  // This can increase your server load as well as your hosting bill.
-  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-side errors will fail.
-  tunnelRoute: "/monitoring",
-
-  // Fix for Vercel ENOENT middleware.js.nft.json trace error
-  autoInstrumentMiddleware: false,
-});
+export default nextConfig;
